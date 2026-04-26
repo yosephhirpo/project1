ae3c0ef4c4b3ffb87a8dbd036f8fb904e09d1a5d
Emo <yosephhirpo@gmail.com>
2026-04-26 13:31:46 +0300

    bankaccount

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        SavingsAccount myAccount = new SavingsAccount("Alex", 1000.0);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        myAccount.deposit(500);
+        myAccount.displayInfo();
+    }
+}
+class BankAccount {
+    private String accountHolder;
+    protected double balance;
+    public BankAccount(String name, double openingBalance) {
+        this.accountHolder = name;
+        this.balance = openingBalance;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance = balance + amount;
+            System.out.println("Deposited: $" + amount);
         }
+    }
+    public void displayInfo() {
+        System.out.println("Holder: " + accountHolder + " | Balance: $" + balance);
+    }
+}
+class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(String name, double balance) {
+        super(name, balance);
     }
 }
